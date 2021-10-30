92fb457ae42f9cb368d4b6cedd722f245cef920f
charlie1099 <cdcharlesworth@gmail.com>
2021-10-30 12:08:00 -0600

    missed a require

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 // import the Sequelize constructor from the library
 const Sequelize = require("sequelize");
 
+require("dotenv").config();
+
 //create connection to our database, pass in the MYSQL info for username and password
 const sequelize = new Sequelize("hike_db", "username", "password", {
     host: "root",
     dialect: "1099758CDc)",
-    port: 3001
+    port: 3306
 });
 
 module.exports = sequelize;
