ef9cd05bfb08c3f53c934885a920154f7e984063
charlie1099 <cdcharlesworth@gmail.com>
2021-11-02 17:00:47 -0600

    working on post rutes

--- models/User.js
+++ models/User.js
@@ -1,15 +1,14 @@
-const { Model, DataTypes } = require("sequelize");
-const sequelize = require("../config/connection");
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-//creaate the model
 class User extends Model {}
 
 User.init(
     {
-
         id: {
             type: DataTypes.INTEGER,
-            allowNull: false,
+            allownull: false,
             primaryKey: true,
             autoIncrement: true
         },
@@ -17,21 +16,29 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false
         },
-        email: {
+        password: {
             type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [8]
+            }
         }
-
+        
     },
     {
-        sequelize,
-        timestamps: false,
-        freezeTableName: true,
-        underscored: true,
-        modelName: "user"
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.passwrod, 10);
+                return newUserData;
+                }
+            },
+            sequelize,
+            timestamps: false,
+            freezeTableName: true,
+            underscored: true,
+            modelName: 'user'
     }
+);
 
 
-
-)
-
-module.exports = User
+module.exports.User;
