0fcb3b54668df1f62ca1e3c0589d30032a97bca9
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2021-11-03 18:53:46 -0600

    added handlebars/refactor

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helper');
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hiking = require('./data/hiking.json');
+const hbs = exphbs.create({ helpers });
 
-app.use(express.static('public'));
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-app.get('/api/hiking', (req, res) => {
-    res.json(hiking);
-});
+const sess = {
+  secret: 'Super secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
+app.use(session(sess));
 
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/public')));
 
+app.use(require('./controllers/'));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
 });
