45b987dae7bc4722dac20a92ba37123e8ce51af9
Sheldon Z Collins <84816623+Sunrisedad@users.noreply.github.com>
2021-11-07 09:31:38 -0700

    added more CSS

---
+++ javascript/add-post.js
@@ -0,0 +1,25 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const username = document.querySelector('input[name="username"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        username
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
